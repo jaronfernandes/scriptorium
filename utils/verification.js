180b52dc68225c04b65bdac6113116bd0c936e95
carmen-chau <80921817+carmen-chau@users.noreply.github.com>
2024-10-18 17:20:26 -0400

    initial middleware file

---
+++ utils/verification.js
@@ -0,0 +1,20 @@
+// Middleware function to verify JWT
+import jwt from "jsonwebtoken";
+
+// Function to verify user access
+// Here, an individual is an user IF they have role "USER" or "ADMIN"
+export function verifyUser(req, res){
+    // Check if we have a valid user
+    const user = verifyToken(req.headers.authorization);
+  
+    if (!user) {
+          return res.status(401).json({
+          message: "Unauthorized",
+          });
+    }
+    // Check if the user has the 'ADMIN' role
+    if (user.role !== "USER" && user.role !== "ADMIN") {
+      return res.status(403).json({ error: "Forbidden: Only users and admins can access this." }); // Forbidden for non-admins
+    }
+    return true;
+  }

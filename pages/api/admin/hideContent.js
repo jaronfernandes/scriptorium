c8694c18e5ad89ecb5a3bf83b40c7b4b04c2ec0f
carmen-chau <80921817+carmen-chau@users.noreply.github.com>
2024-10-19 00:26:15 -0400

    validation checks

--- pages/api/admin/hideContent.js
+++ pages/api/admin/hideContent.js
@@ -25,4 +25,53 @@ export default async function handler (req, res){
     if (req.method !== "POST"){
         return res.status(405).json({error: "Method not supported"});
     }
+    // Getting POST body content
+    const{contentType, blogPostId, commentId} = req.body;
+    
+    // Check if required fields are defined
+    // Using the ? operator in case either variable are undefined
+    if(!contentType?.trim()){
+         return res.status(400).json({ error: "Missing required fields" });
+    }
+ 
+    // Check if contentType is neither of the accepted types 
+    if (contentType !== "BlogPost" && contentType !== "Comment" ) {
+         return res.status(400).json({ error: "Invalid content type. Must be reporting either a 'BlogPost' or 'Comment'." });
+    }
+ 
+    // Check for mismatch between contentType and the ids
+    if (contentType === "BlogPost" && !blogPostId) {
+         return res.status(400).json({ error: "Content type is Blog Post, but missing Blog Post Id" });
+    } 
+     
+    if (contentType === "Comment" && !commentId) {
+         return res.status(400).json({ error: "Content type is Comment, but missing Comment Id" });
+    } 
+
+     // Try block #1:
+    // Validating whether blogPostId / commentId are valid
+    try {
+        if (contentType === "BlogPost"){
+            const searchCondition = {id: blogPostId};
+            const blogPostInstance = await prisma.blogPost.findUnique({
+                where: searchCondition
+            });
+            if (!blogPostInstance){
+                return res.status(404).json({ error: "Blog Post not found" });
+            }
+        } else if (contentType === "Comment"){
+            const searchCondition = {id: commentId};
+            const commentInstance = await prisma.comment.findUnique({
+                where: searchCondition
+            });
+            if (!commentInstance){
+                return res.status(404).json({ error: "Comment not found" });
+            }
+        }
+
+    } catch (error) {
+        // console.error("Error validating content ids:", error); // For debugging purposes
+        return res.status(500).json({ error: "Failed to validate content ids" });
+    }
+
 }

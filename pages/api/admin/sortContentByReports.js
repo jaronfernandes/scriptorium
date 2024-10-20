9107f5cdf4381ffa77a245b0bb5b69198d25075e
carmen-chau <80921817+carmen-chau@users.noreply.github.com>
2024-10-20 01:52:03 -0400

    added tentative code

--- pages/api/admin/sortContentByReports.js
+++ pages/api/admin/sortContentByReports.js
@@ -29,8 +29,54 @@ export default async function handler (req, res){
     }
     // Getting GET body content
     //TODO: Anything else to extract?. 
-    // For now, assyume contentType can be: "blogPosts", "comments", or "both" 
+    // For now, assyume contentType can be: "BlogPost", "Comment". Not supporting the "Both" option as of now
     const {contentType} = req.query;
 
-    // TODO: Continue implementation. Refer to @192 on Piazza for more details
+    // Validating req.query entry
+    // Check if required fields are defined
+    // Using the ? operator in case either variable are undefined
+    if(!contentType?.trim() || (contentType !== "BlogPost" && contentType !== "Comment")){
+        return res.status(400).json({ error: "Missing required fields" });
+    }
+
+    // Try statement to retrieve all non-hidden blog posts AND all non-hidden comments
+    try {
+        // Defining common returned variable
+        let retrievedContent = [];
+        // Defining common search condition
+        const contentSearchCondition = {hidden:false}
+
+        // Fetching all non-hidden blog posts
+        if (contentType === "BlogPost"){
+            retrievedContent = await prisma.blogPost.findMany({
+                where: contentSearchCondition,
+                include: { // creates an additional nested object to store the number of reports a blog post garnered
+                    _count: {
+                        select: { reports: true }
+                    }
+                }
+            });
+
+        }
+
+        // Fetching all non-hidden comments
+        if (contentType === "Comment"){
+            retrievedContent = await prisma.comment.findMany({
+                where: contentSearchCondition,
+                include: { // creates an additional nested object to store the number of reports a comment garnered
+                    _count: {
+                        select: { reports: true } 
+                    }
+                }
+            });
+        }
+        // Sort the entries in descending order from having the most reports to having the least reports 
+        // TODO: How to do this?
+        
+        // Return the results
+        return res.status(200).json(retrievedContent);
+    } catch (error) {
+        // console.error("Error fetching and/or sorting content:", error); // For debugging purposes
+        return res.status(500).json({ error: "Failed to fetch or sort content" });
+    }
 }

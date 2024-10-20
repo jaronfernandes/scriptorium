7288cbe94c5614252e1ff56a8bac89f0c2a85ee9
carmen-chau <80921817+carmen-chau@users.noreply.github.com>
2024-10-20 09:46:47 -0400

    initial file

---
+++ pages/api/execute_code.js
@@ -0,0 +1,17 @@
+/*
+    User story to satisfy:
+
+        1. As a visitor, I want to execute my code on Scriptorium and see the output in real-time so that I can quickly verify its correctness.
+
+
+    Assumptions:
+
+    From Piazza note @220, just assume that ALL the user input is given in 1 go
+    Ie: Thinking of the program running as: You give it all the input at once, and it gives you all the output at once
+    
+    
+    Note:
+
+        - Did not put this in a nested folder (eg: user or admin) because this route is accessible to all (accesible to the lowest level, visitor)
+
+*/

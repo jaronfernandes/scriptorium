31949fbe85e672319c4189771ba748363adf0e2b
fern1394 <fernandesjaron@gmail.com>
2024-11-02 05:09:23 -0400

    worked on viewing/searching for templates use case under code templates

--- pages/api/templates/viewSearchTemplate.js
+++ pages/api/templates/viewSearchTemplate.js
@@ -1,13 +1,25 @@
 import prisma from '../../../../utils/db';
 import { verifyAccessToken, generateRefreshToken } from '../../../../utils/auth';
 
+
+/*
+As a user, I want to view and search through my list of my saved templates, 
+including their titles, explanations, and tags, so that I can easily find and reuse them.
+*/
+
+/**
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ * @returns 
+ */
 export default async function handler(req, res) {
     if (req.method !== 'GET') {
         return res.status(405).json({ message: "Must be a GET request." });
     }
 
     const accessToken = req.headers.authorization;
-    const { accessToken } = req.body;
+    const { title, explanation, tags } = req.query;
 
     const verified_token = verifyAccessToken(accessToken);
     if (!verified_token) {
@@ -15,15 +27,38 @@ export default async function handler(req, res) {
     }
 
     try {
-       const templates = await prisma.codeTemplate.findMany({
-            where: {
-                user: {
-                    username: verified_token.username
+        const filter_settings = {
+            authorId: verified_token.userId,
+        }
+        if (title) {
+            filter_settings.title = {
+                contains: title,
+                mode: "insensitive"
+            }
+        }
+        if (explanation) {
+            filter_settings.explanation = {
+                contains: explanation,
+                mode: "insensitive"
+            }
+        }
+        // Finds if at least one of the tags is in the given list of tags to find matches with.
+        // TODO: Could make it more strict by allowing only templates that have all the tags in the list.
+        if (tags) {
+            filter_settings.tags = {
+                some: {
+                    name: {
+                        in: tags,
+                    }
                 }
             }
+        }
+
+       const templates = await prisma.codeTemplate.findMany({
+            where: filter_settings
         });
-        return res.status(200).json({ "templates": templates
-       })
+
+        return res.status(200).json({ "templates": templates });
     } catch (error) {
         return res.status(401).json({ "error": error.message });
     }

6278baabbc063aeda29e0fb6915302666c845fa5
fern1394 <fernandesjaron@gmail.com>
2024-11-02 05:31:38 -0400

    worked on edittemplate.js, should be done i hope

--- pages/api/templates/editTemplate.js
+++ pages/api/templates/editTemplate.js
@@ -1,28 +1,68 @@
 import prisma from '../../../../utils/db';
 import { verifyAccessToken, generateRefreshToken } from '../../../../utils/auth';
 
+/*
+As a user, I want to edit an existing code template’s title, explanation, tags, and code, or delete it entirely.
+*/
+
+/**
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ */
 export default async function handler(req, res) {
-    if (req.method !== 'GET') {
-        return res.status(405).json({ message: "Must be a GET request." });
-    }
+    if (req.method === 'PUT') {
+        const accessToken = req.headers.authorization;
+        const { title, explanation, tags, code, templateId } = req.body;
+        const updates = {};
 
-    const token = req.headers.authorization;
-
-    if (!auth_header) {
-        return res.status(401).json({ message: 'No Token' });
-    }
-
-    try {
-        const verified_token = verifyAccessToken(token);
-
+        const verified_token = verifyAccessToken(accessToken);
         if (!verified_token) {
-            return res.status(401).json({ error: "Invalid token" });
+            return res.status(401).json({ error: "Unauthorized" });
         }
-    } catch (error) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
 
-    /**
-     * Assume: title: String, explanation: String, tags: String[], code: String, refreshToken: String
-     */
+        if (title) {
+            updates.title = title;
+        }
+        if (explanation) {
+            updates.explanation = explanation;
+        }
+        if (tags) {
+            updates.tags = tags;
+        }
+        if (code) {
+            updates.code = code;
+        }
+
+        const updatedTemplate = await prisma.codeTemplate.update({
+            where: {
+                id: parseInt(templateId),
+            },
+            data: updates
+        });
+
+        res.status(200).json(updatedTemplate);
+    } else if (req.method === 'DELETE') {
+        const accessToken = req.headers.authorization;
+        const { templateId } = req.body;
+
+        const verified_token = verifyAccessToken(accessToken);
+        if (!verified_token) {
+            return res.status(401).json({ error: "Unauthorized" });
+        }
+
+        try {
+            await prisma.codeTemplate.delete({
+                where: {
+                    id: parseInt(templateId),
+                }
+            });
+            res.status(200).json({ message: "Template deleted successfully." });
+        } catch (error) {
+            // Likely failed to delete the code template
+            res.status(500).json({ error: error.message });
+        }
+    } else {
+        res.status(405).json({ message: "Method must be a PUT or DELETE request." });
+    }
 }

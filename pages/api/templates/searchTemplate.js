401ec3e0d449f6f91e68135aaa0a804adee2f312
fern1394 <fernandesjaron@gmail.com>
2024-11-03 03:43:29 -0500

    fixed searchTemplate.js, but still need to do pagination for this

--- pages/api/templates/searchTemplate.js
+++ pages/api/templates/searchTemplate.js
@@ -1,4 +1,4 @@
-import prisma from '../../../../utils/db';
+import prisma from '../../../utils/db';
 
 /*
 As a visitor, I want to search through all available templates by title, 
@@ -21,12 +21,17 @@ export default async function handler(req, res) {
     try {
         const filter_settings = {};
 
+        console.log("hi");
+        console.log(title, tags, content);
+
         if (title) {
             filter_settings.title = {
                 contains: title,
-                mode: "insensitive"
             }
         }
+
+        console.log("hi2");
+        console.log(filter_settings);
 
         if (tags) {
             filter_settings.tags = {
@@ -37,21 +42,27 @@ export default async function handler(req, res) {
                 }
             }
         }
+
+        console.log("hi3");
         // this could mean explanations, but ill assume code for now 
         // since technically code is the content of the code template
         if (content) {
             filter_settings.code = {
                 contains: content,
-                mode: "insensitive"
             }
         }
 
-        const templates = await prisma.template.findMany({
+        console.log("hi4");
+        console.log(filter_settings);
+
+        const templates = await prisma.codeTemplate.findMany({
             where: filter_settings
         });
 
+        console.log("hi5");
+
         return res.status(200).json(templates);
     } catch (error) {
-        return res.status(500).json({ error: "Internal server error." });
+        return res.status(500).json({ error: error.message });
     }
 }

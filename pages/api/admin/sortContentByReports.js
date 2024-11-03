40e43f1319c571f9a7b46e487f895d561e92d651
carmen-chau <80921817+carmen-chau@users.noreply.github.com>
2024-11-03 01:27:39 -0500

    small change

--- pages/api/admin/sortContentByReports.js
+++ pages/api/admin/sortContentByReports.js
@@ -3,7 +3,7 @@
     "As the system administrator, I want to sort blog posts and comments based on the total number of reports 
     they received so that I can find the inappropriate content easier."
 
-    Overall purpose of the file: Creates an GET endpoint that takes user info fron req.query
+    Overall purpose of the file: Creates an GET endpoint that takes user info fron req.query.
     and sorts content.
 
     TODO: Massive implementation assumption (permitted based on Piazza post @192). Assuming that the sorted page displays

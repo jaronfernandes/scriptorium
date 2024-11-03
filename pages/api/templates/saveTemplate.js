9651eb7745f5cc0cc7b9d7debd00093c3165387e
fern1394 <fernandesjaron@gmail.com>
2024-11-03 15:26:44 -0500

    added error message return if any of the fields aren't entered in saveTemplate.js

--- pages/api/templates/saveTemplate.js
+++ pages/api/templates/saveTemplate.js
@@ -19,6 +19,8 @@ export default async function handler(req, res) {
 
     if (!verified_token) {
         return res.status(401).json({ error: "Invalid token" });
+    } else if (!title || !explanation || !tags || !code || !language) {
+        return res.status(400).json({ error: "All fields title, explanation, tags, code, and language are required." });
     }
 
     const user = await prisma.user.findUnique({
@@ -39,42 +41,6 @@ export default async function handler(req, res) {
     try {
         // const newTags = []
         console.log(tags);
-
-        // for (let tagName of tags) {
-        //     let tag = await prisma.tag.findUnique({
-        //         where: { name: tagName }
-        //     });
-
-        //     if (!tag) {
-        //         tag = await prisma.tag.create({
-        //             data: { name: tagName }
-        //         });
-        //     }
-        //     newTags.push(tag.id);
-        // }
-
-        // const newTags = await Promise.all(
-        //     tags.map(async (tag) => {
-        //         let newTag = await prisma.tag.findUnique({
-        //             where: { 
-        //                 name: tag 
-        //             }
-        //         });
-
-        //         if (!newTag) {
-        //             newTag = await prisma.tag.create({
-        //                 data: { name: tag }
-        //             })
-        //         }
-                
-        //         return newTag.id;
-
-        //         // return existingTag
-        //         //     ? existingTag.id
-        //         //     : await prisma.tag.create({ data: { name: tag } }).then(tag => tag.id);
-        //     })
-        // )
-
         console.log("newTags");
 
         const newTags = await Promise.all(
@@ -111,17 +77,6 @@ export default async function handler(req, res) {
           if (!template) {
             return res.status(500).json({ error: "Failed to save template" });
           }
-          
-          
-        // // after tags and templates are created (or existing), create the relationship between the two.
-        // await prisma.codeTemplateTag.createMany({
-        //     data: newTags.map(tag => ({
-        //         templateId: template.id,
-        //         tagId: tag,  // don't do id since newTags is an array of tag ids already
-        //         assignedBy: user.firstName // Store who assigned the tag, if needed
-        //     }))
-        // });
-
         return res.status(201).json({ savedTemplate: template });
     } catch (error) {
         return res.status(400).json({ error: error.message });
